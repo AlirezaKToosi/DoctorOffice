b1769c89d15fa8c297c19d1dd0ba9dad7ea3413e
AlirezaKToosi <71192730+AlirezaKToosi@users.noreply.github.com>
2022-04-25 14:39:18 +0200

    initial commit

---
+++ src/main/java/com/doctoroffice/DoctorofficeApplication.java
@@ -0,0 +1,13 @@
+package com.doctoroffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doctoroffice/DoctorofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctoroffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
