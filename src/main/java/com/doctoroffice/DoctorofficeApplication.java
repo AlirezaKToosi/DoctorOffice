fad935450d042e8ecf2568fb8d9d99ac4e7fdb25
AlirezaKToosi <71192730+AlirezaKToosi@users.noreply.github.com>
2022-04-28 23:58:27 +0200

    Creates Main Class
    Creates main class with springBootApplication and AutoWired annotation

--- src/main/java/com/doctoroffice/DoctorofficeApplication.java
+++ src/main/java/com/doctoroffice/DoctorofficeApplication.java
@@ -1,13 +1,19 @@
 package com.doctoroffice;
 
+import com.doctoroffice.entity.PatientEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DoctorofficeApplication {
+public class DoctorofficeApplication  {
+    @Autowired
+    PatientRepository repository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DoctorofficeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorofficeApplication.class, args);
+    }
+
 
 }
