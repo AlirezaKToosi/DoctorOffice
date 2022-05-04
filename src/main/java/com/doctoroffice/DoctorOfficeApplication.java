04895fabb0ead4b0c7891d8ce908225ccda845fe
AlirezaKToosi <71192730+AlirezaKToosi@users.noreply.github.com>
2022-05-05 00:13:53 +0200

    Removes CommandLineRunner Interface implementation

--- src/main/java/com/doctoroffice/DoctorOfficeApplication.java
+++ src/main/java/com/doctoroffice/DoctorOfficeApplication.java
@@ -1,19 +1,24 @@
 package com.doctoroffice;
 
 import com.doctoroffice.entity.PatientEntity;
+import com.doctoroffice.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DoctorofficeApplication  {
+public class DoctorOfficeApplication /*implements CommandLineRunner*/ {
     @Autowired
     PatientRepository repository;
 
     public static void main(String[] args) {
-        SpringApplication.run(DoctorofficeApplication.class, args);
+        SpringApplication.run(DoctorOfficeApplication.class, args);
     }
 
 
+//    @Override
+//    public void run(String... args) throws Exception {
+//       // repository.insert(new PatientEntity("1234567899"));
+//    }
 }
