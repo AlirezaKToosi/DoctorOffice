2990d1751906538c1538519c558746b568514312
AlirezaKToosi <71192730+AlirezaKToosi@users.noreply.github.com>
2022-05-05 00:23:28 +0200

    There is not any changes here

--- src/test/java/com/doctoroffice/DoctorOfficeApplicationTests.java
+++ src/test/java/com/doctoroffice/DoctorOfficeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DoctorofficeApplicationTests {
+class DoctorOfficeApplicationTests {
 
 	@Test
 	void contextLoads() {
