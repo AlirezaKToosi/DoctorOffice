9da386dfc806e7ed3681b0c8f7de8450e61e65b3
AlirezaKToosi <71192730+AlirezaKToosi@users.noreply.github.com>
2022-05-06 12:42:05 +0200

    Adds ApiError
    Adds this class for implementing ErrorHandler

---
+++ src/main/java/com/doctoroffice/service/ApiError.java
@@ -0,0 +1,31 @@
+package com.doctoroffice.service;
+
+import org.springframework.http.HttpStatus;
+
+import java.lang.reflect.Array;
+import java.net.http.HttpRequest;
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiError {
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+
+    public ApiError(HttpStatus status, String message, List<String> errors) {
+        super();
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+    public ApiError(HttpStatus status, String message, String error) {
+        super();
+        this.status = status;
+        this.message = message;
+        errors = Arrays.asList(error);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
