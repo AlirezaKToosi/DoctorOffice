a5c5ffb0dc4acff138041897609082de86604e40
AlirezaKToosi <71192730+AlirezaKToosi@users.noreply.github.com>
2022-05-21 00:55:47 +0200

    Using DTO and ResponseEntity
    Using DTO and ResponseEntity

--- src/main/java/com/doctoroffice/controller/PatientController.java
+++ src/main/java/com/doctoroffice/controller/PatientController.java
@@ -1,14 +1,21 @@
 package com.doctoroffice.controller;
 
+import com.doctoroffice.dto.PatientIgnoreMixin;
 import com.doctoroffice.entity.PatientEntity;
 import com.doctoroffice.service.PatientService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 //mark class as Controller
 @RestController
+//@RequestMapping("/api")
 public class PatientController {
     //Autowired the PatientService class
     @Autowired
@@ -16,14 +23,22 @@ public class PatientController {
 
     //creating a get mapping that retrieves all the patients detail from the database
     @GetMapping("/patient")
-    private List<PatientEntity> getAllPatient() {
-        return patientService.getAllPatient();
+    private ResponseEntity<?> getAllPatients() throws JsonProcessingException {
+        List<PatientEntity> patientList=patientService.getAllPatient();
+        ObjectMapper mapper=new ObjectMapper();
+        mapper.addMixIn(PatientEntity.class, PatientIgnoreMixin.class);
+        String jsonString= mapper.writeValueAsString(patientList);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(jsonString);
     }
 
     //creating a get mapping that returns the specific patient with it's id from database
     @GetMapping("/patient/{id}")
-    private PatientEntity getPatientById(@PathVariable("id") Integer id) throws Exception {
-        return patientService.getById(id);
+    private ResponseEntity getPatientById(@PathVariable("id") Integer id) throws Exception {
+        PatientEntity patientEntity=patientService.getById(id);
+        ObjectMapper mapper=new ObjectMapper();
+        mapper.addMixIn(PatientEntity.class,PatientIgnoreMixin.class);
+        String JsonString=mapper.writeValueAsString(patientEntity);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(JsonString);
     }
 
     //creating post mapping that save new patient detail in the database
@@ -39,7 +54,13 @@ public class PatientController {
     }
 
     @DeleteMapping("/patient/{id}")
-    private void deleteById(@PathVariable("id") Integer id) throws Exception {
-        patientService.deleteById(id);
+    private ResponseEntity<String> deleteById(@PathVariable("id") Integer id){
+        try {
+            patientService.deleteById(id);
+            return new ResponseEntity<>("Patient with id"+id+"has been deleted", HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Patient with this id is not exist",HttpStatus.NOT_FOUND);
+        }
     }
 }
