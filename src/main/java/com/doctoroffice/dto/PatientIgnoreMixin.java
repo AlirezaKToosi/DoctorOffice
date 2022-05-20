6c580bf4b3ff9340dd7e118baacee6835968a7c1
AlirezaKToosi <71192730+AlirezaKToosi@users.noreply.github.com>
2022-05-21 00:57:04 +0200

    ADDs DTO class
    add patirntignorclass

---
+++ src/main/java/com/doctoroffice/dto/PatientIgnoreMixin.java
@@ -0,0 +1,10 @@
+package com.doctoroffice.dto;
+
+import com.doctoroffice.entity.PatientEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public abstract class PatientIgnoreMixin {
+    @JsonIgnore
+    public abstract Integer getId();
+
+}
