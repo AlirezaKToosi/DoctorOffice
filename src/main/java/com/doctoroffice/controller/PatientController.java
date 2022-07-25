eaa617a7b73848bc4ba4d6a790cfdb0d21ec4d6f
AlirezaKToosi <71192730+AlirezaKToosi@users.noreply.github.com>
2022-07-26 01:55:23 +0200

    Changes in Patient Controller remove previous mapper using response and request object

--- src/main/java/com/doctoroffice/controller/PatientController.java
+++ src/main/java/com/doctoroffice/controller/PatientController.java
@@ -32,50 +32,64 @@ public class PatientController {
      creating a get mapping that retrieves all the patients detail from the database
      */
     @GetMapping("/patient")
-    private ResponseEntity<?> getAllPatients() throws JsonProcessingException {
-        List<PatientEntity> patientList=patientService.getAllPatient();
-        ObjectMapper mapper=new ObjectMapper();
-        mapper.addMixIn(PatientEntity.class, PatientIgnoreMixin.class);
-        String jsonString= mapper.writeValueAsString(patientList);
-        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(jsonString);
+    private ResponseEntity<?> getAllPatients(){
+        List<RegisterPatientResponse> patientList=patientService.getAllPatient();
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(patientList);
     }
 
     /**
      creating a get mapping that returns the specific patient with it's id from database
      */
     @GetMapping("/patient/{id}")
-    private ResponseEntity getPatientById(@PathVariable("id") Integer id) throws Exception {
-        PatientEntity patientEntity=patientService.getById(id);
-        ObjectMapper mapper=new ObjectMapper();
-        mapper.addMixIn(PatientEntity.class,PatientIgnoreMixin.class);
-        String JsonString=mapper.writeValueAsString(patientEntity);
-        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(JsonString);
+    private ResponseEntity getPatientById(@PathVariable("id") String id)  {
+        RegisterPatientResponse patientEntity= null;
+        try {
+            patientEntity = patientService.getById(id);
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(patientEntity);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Patient with this NationalId Does not exist",HttpStatus.NOT_FOUND);
+        }
     }
 
         /**
          creating post mapping that save new patient detail in the database
          */
     @PostMapping("/patient")
-    private void savePatient(@RequestBody RegisterPatientRequest patient) {
-        patientService.saveOrUpdate(patient);
+    private ResponseEntity savePatient(@RequestBody RegisterPatientRequest patient){
+        try {
+            patientService.saveOrUpdate(patient);
+            return new ResponseEntity<>("Patient has been added to database",HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Patient with this information can not add to database",HttpStatus.NOT_FOUND);
+        }
     }
 
         /**
           creating put mapping that update the patient detail in the database
          */
     @PutMapping("/patient")
-    private void updatePatient(@RequestBody RegisterPatientRequest patient) {
-        patientService.saveOrUpdate(patient);
+    private ResponseEntity updatePatient(@RequestBody RegisterPatientRequest patient) {
+        try {
+            patientService.saveOrUpdate(patient);
+            return new ResponseEntity<>("Patient has been updated",HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Patient with this information can not be updated",HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/patient/{id}")
-    private ResponseEntity<String> deleteById(@PathVariable("id") Integer id){
+    private ResponseEntity<String> deleteById(@PathVariable("id") String id){
         try {
             patientService.deleteById(id);
-            return new ResponseEntity<>("Patient with id"+id+"has been deleted", HttpStatus.ACCEPTED);
+            return new ResponseEntity<>("Patient with NationalId "+id+" has been deleted", HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>("Patient with this id is not exist",HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("Patient with this NationalId Does not exist",HttpStatus.NOT_FOUND);
         }
     }
 }
